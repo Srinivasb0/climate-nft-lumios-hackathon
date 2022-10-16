8e8e1829019cbf2e8c67251bee79f4cd498b9cbd
srinivas_b <srinib897@gmail.com>
2022-10-16 16:19:41 +0530

    Update revise-automate.js

--- Scripts/revise-automate.js
+++ Scripts/revise-automate.js
@@ -1,6 +1,6 @@
 // const { Revise } = require("revise-sdk");
 import { Revise } from "revise-sdk";
-const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImZjM2ZhMzg0LTJmMzYtNDhlNC1hYjI3LTI2MmIwZjJlNWZhMyIsImtleSI6IjB4OXExMG00IiwiaWF0IjoxNjY0MTQzNDQzfQ.sZAITgYVoi-27sayi6gTR5W4PGgHNybyvjNEW1CfXck"; //this needs to be replaced by the AUTH TOKEn you generate
+const AUTH_TOKEN = "???"; //this needs to be replaced by the AUTH TOKEn you generate
 const revise = new Revise({auth: AUTH_TOKEN});
 
 const avg_temp = 19;
@@ -9,7 +9,7 @@ const number_days = 20
 async function run() {
 
     if(avg_temp > 18 && avg_temp <20.5) {
-      const nft = await revise.fetchNFT('4a43dcc9-900e-4a26-b458-beb93fee7495');
+      const nft = await revise.fetchNFT('nft-id');
       const nftobject = revise.nft(nft);
       nftobject.setProperty('GrowthPeriod', 'Emergence to Flower').save();
       console.log(nftobject);
